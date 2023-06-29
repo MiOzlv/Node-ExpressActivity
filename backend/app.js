a9cb3741dc150577055bdfb2a5c4db0fce2cf87f
MiOzlv <mortegava0001@gmail.com>
2023-06-28 17:35:18 -0700

    Express-Node

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,10 @@ const express= require('express');
 const fs = require( 'fs');
 
 const server = express();
-const PORT = 8080;
+const PORT = 3000;
 
 app.use('/home', (req, res)=>{
-    fs.readFile('../frontend/home.html', (err, data)=>{
+    fs.readFile('../frontend/contact.html', (err, data)=>{
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         return res.end();
